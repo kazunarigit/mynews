32cd3dc1e8752116c4aa6fe73513ccd025e92ce4
murata <kazunari.loto6@gmail.com>
2022-04-03 09:09:46 +0000

    heroku

---
+++ app/News.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    // 以下を追記
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'title' => 'required',
+        'body' => 'required',
+    );
+    
+     public function histories()
+    {
+        return $this->hasMany('App\History');
+
+    }
+}
---
+++ resources/views/admin/profile/profile.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>プロフィール</title>
+    </head>
+    <body>
+        <h1>名前</h1>
+        <h1>年齢</h1>
+        <h1>住所</h1>
+    </body>
+</html>
