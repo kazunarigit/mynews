0248851dd3691212926d878febeaf0cda90bf800
murata <kazunari.loto6@gmail.com>
2022-04-09 05:58:16 +0000

    PHP09課題４

--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create'); # 追記
-});
-Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/delete', 'Admin\NewsController@delete');
     Route::get('profile/create', 'Admin\ProfileController@add');
-});
-Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
-    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); 
-    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
-    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
-    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
-});
 Route::get('/', 'NewsController@index');
