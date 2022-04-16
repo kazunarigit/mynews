e57c63a6f9e2002e4705ba588440facf0d445e5c
murata <kazunari.loto6@gmail.com>
2022-04-16 13:56:55 +0000

    PHP14課題4～

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -11,7 +11,7 @@ class ProfileController extends Controller
     public function add(){
         return view('admin.profile.create');
     }
-    public function create(){
+    public function create(Request $request){
         
         // 以下を追記
       // Varidationを行う
@@ -19,19 +19,6 @@ class ProfileController extends Controller
 
       $profile = new Profile;
       $form = $request->all();
-
-    
-    if (isset($form['image'])) {
-       $path = Storage::disk('s3')->putFile('/',$news_form['image'],'public');
-       $news->image_path = Storage::disk('s3')->url($path);
-    } else {
-        $news->image_path = null;
-    }
-
-      // フォームから送信されてきた_tokenを削除する
-      unset($form['_token']);
-      // フォームから送信されてきたimageを削除する
-      unset($form['image']);
 
       // データベースに保存する
       $profile->fill($form);
---
+++ database/migrations/2022_04_16_132735_create_profile_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name'); //名前
+            $table->string('gender');  //性別
+            $table->string('hobby'); // 趣味
+            $table->string('introduction');  // 自己紹介
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
