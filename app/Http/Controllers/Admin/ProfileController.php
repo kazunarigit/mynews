e83c5c4d689d7640c9821610c75396e4d8af1ec9
murata <kazunari.loto6@gmail.com>
2022-04-16 09:44:58 +0000

    PHP14課題14r

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -12,6 +12,32 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     public function create(){
+        
+        // 以下を追記
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+
+    
+    if (isset($form['image'])) {
+       $path = Storage::disk('s3')->putFile('/',$news_form['image'],'public');
+       $news->image_path = Storage::disk('s3')->url($path);
+    } else {
+        $news->image_path = null;
+    }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+      
+     
         return redirect('admin/profile/create');
     }
     public function edit(){
@@ -21,6 +47,5 @@ class ProfileController extends Controller
         return redirect('admin/profile/edit');
     }
     
-    $profile->fill($form);
-    $profile->save;
+
 }
