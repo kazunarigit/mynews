9712434473ebc761c493684a8ac7dbc6b19a24ad
murata <kazunari.loto6@gmail.com>
2022-07-27 02:12:38 +0000

    ragistration

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -50,6 +50,7 @@
                             <!-- Authentication Links -->
                         {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
                         @guest
+                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                             <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                         {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
                         @else
